41ed5b9b38b9f4d08c029a30dd29a70ccf901ab8
Jean Carlos <jeancarlos.sotobarco@gmail.com>
2018-06-06 21:58:50 -0500

    Ejercicios de listas

---
+++ arbol_genologico.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+me = {"name" : "Jean Carlos", "last name" : "Soto", "age" : 27, "dad" : {"name" : "Jhon", "last" : "Soto", "age" : 50}, "mom" : {"name" : "Nora", "last" : "Barco", "age" : 52}, "sister" : {"name" : "Johana", "last" : "Soto", "age" : 30}, "brother" : {"name" : "Snider", "last" : "Soto", "age" : 22}}
+print(me)
+print(me["dad"]["age"])
